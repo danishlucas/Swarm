3ac47291a171444acc7844706f814ecbdfa26681
Brkanac <GAR_lebrkanac@seattleschools.org>
2017-01-06 09:33:32 -0800

    Dash button and settings added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D myBody;
     public float moveForce = 3;
-    public float dashForce = 2;
+    public float dashForce = 50;
+    public int deltaTime1 = 0;
+    public int deltaTime2;
+    public bool isDashing;
+    public bool buttonAvailable = true;
     // Use this for initialization
     void Start()
     {
@@ -17,15 +21,39 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
         bool isBoosting = CrossPlatformInputManager.GetButton("Dash");
+        if (deltaTime1 == 0)
+        {
+            buttonAvailable = true;
+        }
+        if (isBoosting == true && buttonAvailable == true)
+        {
+            deltaTime1 += 1;
+        }
+        if (deltaTime1 >= 1 && deltaTime1 <= 20)
+        {
+            deltaTime1++;
+            isDashing = true;
+            buttonAvailable = false;
+        }
+        else if (deltaTime1 > 20 && deltaTime1 < 50)
+        {
+            isDashing = false;
+            buttonAvailable = false;
+            deltaTime1++;
+        }
+        if (deltaTime1 == 50)
+        {
+            deltaTime1 = 0;
 
-        myBody.velocity = (moveVec * (isBoosting ? dashForce : 1));
+        }
+
+        Debug.Log(deltaTime1);
+        Debug.Log(isDashing);
+
+        myBody.velocity = (moveVec * (isDashing ? dashForce : 1));
         //myBody.velocity = (moveVec * dashForce);
-
 
 
     }
 
-
 }
-
-
