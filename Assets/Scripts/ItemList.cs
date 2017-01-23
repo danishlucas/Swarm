cd4004942906785484bbbcb490502e8cce4d85bb
Lucas <GAR_dhlucas@seattleschools.org>
2017-01-23 09:06:06 -0800

    Updated enemy scripts

---
+++ Assets/Scripts/ItemList.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+//using Boo.Lang;
+
+public class ItemList : MonoBehaviour {
+
+    public GameObject HealItem;
+    public GameObject SpeedUpItem;      // aight so for some reason this doesnt show up, fix it later
+    /*
+     * Make HELLLLLLAAAAAAAA MORE ITEMS BRO!
+     */
+    //public List<GameObject> list;
+
+
+	// Use this for initialization
+	void Start () {
+        //list.Add(HealItem);
+       // list.Add(SpeedUpItem);
+	}
+}
---
+++ Assets/Scripts/OpenWhenTouched.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+//using Boo.Lang;
+
+public class OpenWhenTouched : MonoBehaviour
+{
+
+    public GameObject itemSpawn;
+    public int itemchosen;
+    //public List<GameObject> itemList;
+   
+    void start()
+    {
+       // itemList = new List<GameObject>();
+        //itemList.Add(
+    }
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            // chooseItem;
+        {
+            //GameObject instance = Instantiate(bullet, itemSpawn.transform.position, itemSpawn.transform.rotation) as GameObject;
+            Destroy(gameObject);
+
+
+
+
+
+        }
+    }
+    void chooseItem()
+    {
+
+    }
+}
+
+
