8a223f9f61c0577e00a77e7b46850b8412baea64
Cameron <evilbees@comcast.net>
2017-02-08 09:43:14 -0800

    Added a working health bar to Cam's test scene

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int health = GameObject.FindWithTag("Player").GetComponent<Attributes>().health;
+        this.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * 20);
+    }
+}
