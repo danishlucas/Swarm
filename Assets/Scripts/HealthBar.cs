1f0da0ab6afa9245cac3aee115c874a5897e5d48
Cameron <evilbees@comcast.net>
2017-02-14 11:55:53 -0800

    working on the health bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,16 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        int health = GameObject.FindWithTag("Player").GetComponent<Attributes>().health;
-        this.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, health * 20);
-    }
+    public Sprite[] HealthSprites;
+
+
 }
