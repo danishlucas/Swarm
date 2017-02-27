fb25cedd4aeaf32c855e26b60fdd53c24ce478cb
Cameron <evilbees@comcast.net>
2017-02-27 09:10:21 -0800

    health bar works

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,6 +5,18 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour {
 
     public Sprite[] HealthSprites;
+    public Image HeartUI;
+    private GameObject Player;
+    int health;
 
-
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("PLAYER");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        health = Player.GetComponent<Attributes>().health;
+        HeartUI.sprite = HealthSprites[health];
+	}
 }
