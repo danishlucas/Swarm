a3d5ca81bd41e05b9f867ef8857dc663733f5cea
Lucas <GAR_dhlucas@seattleschools.org>
2017-03-22 09:21:07 -0700

    Welcome room

---
+++ Assets/Scripts/DeleteText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteText : MonoBehaviour {
+
+    public GameObject welcome1;
+    public GameObject welcome2;
+    public GameObject welcome3;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.GetComponent<PlayerInRoom>().InRoom)
+        {
+            Destroy(welcome1);
+            Destroy(welcome2);
+            Destroy(welcome3);
+        }
+	
+	}
+}
--- Assets/Scripts/SendToNextRoom.cs
+++ Assets/Scripts/SendToNextRoom.cs
@@ -14,6 +14,7 @@ public class SendToNextRoom : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        room = gameObject.transform.root.gameObject;
         active = false;
         animation = GetComponent<Animator>();
         
