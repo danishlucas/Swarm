431387a6ebd41942c32bc8609bb9f8e5ca708dda
Cameron <evilbees@comcast.net>
2017-03-29 10:12:47 -0700

    Chest animation updated

---
+++ Assets/ChestOpen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestOpen : MonoBehaviour {
+
+    private Animator animation;
+
+	// Use this for initialization
+	void Start () {
+        animation = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            animation.SetTrigger("OpenChest");
+        }
+    }
+}
