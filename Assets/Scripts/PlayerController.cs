fa0d6b488a02546b96b685fe3d7b5add8b37aa12
Cameron <evilbees@comcast.net>
2017-04-07 09:27:00 -0700

    first commit here

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,12 @@ public class PlayerController : MonoBehaviour
     public int deltaTime1 = 0;
     public int deltaTime2;
     public bool isDashing;
-    public bool buttonAvailable = true;
+    //public bool buttonAvailable = true;
     private Animator animation;
-    public GameObject joystick;
-    public GameObject button;
-    public float fillAmount;
-    private Image cooldown;
+    //public GameObject joystick;
+    //public GameObject button;
+    //public float fillAmount;
+    //private Image cooldown;
 
 
 
@@ -27,31 +27,27 @@ public class PlayerController : MonoBehaviour
         animation = GetComponent<Animator>();
 
         myBody = this.GetComponent<Rigidbody2D>();
-        cooldown = button.GetComponent<Image>();
+        //cooldown = button.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        float xPos = joystick.GetComponent<RectTransform>().anchoredPosition.x;
-        float yPos = joystick.GetComponent<RectTransform>().anchoredPosition.y;
+        //float xPos = joystick.GetComponent<RectTransform>().anchoredPosition.x;
+        //float yPos = joystick.GetComponent<RectTransform>().anchoredPosition.y;
 
-        float xRotation = (float)(Math.Atan2(xPos, yPos)*(180/3.1415)*-1);
-       // Debug.Log(xRotation);
-        //Vector3 rotation = new Vector3(0,0 , xRotation);
-        transform.localEulerAngles = new Vector3(0, 0, xRotation);
-        //transform.Rotate(rotation);
+        //float xRotation = (float)(Math.Atan2(xPos, yPos)*(180/3.1415)*-1);
+        transform.localEulerAngles = new Vector3(0, 0, 0/*xRotation*/);
 
 
         Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
-        bool isBoosting = CrossPlatformInputManager.GetButton("Dash");
         bool keyboardDash = Input.GetButtonDown("KeyboardDashButton");
-        if (deltaTime1 == 0)
-        {
-            buttonAvailable = true;
-        }
-        if ((isBoosting || keyboardDash) && buttonAvailable)
+        //if (deltaTime1 == 0)
+        //{
+          //  buttonAvailable = true;
+        //}
+        if (keyboardDash)
         {
             deltaTime1 += 1;
             animation.SetTrigger("Dash");
@@ -61,18 +57,12 @@ public class PlayerController : MonoBehaviour
         if (deltaTime1 >= 1 && deltaTime1 <= 20)
         {
             deltaTime1++;
-            isDashing = true;
-            buttonAvailable = false;
-            cooldown.fillAmount += .04f;
-            cooldown.fillAmount = 0;
-            
+            isDashing = true;           
         }
         else if (deltaTime1 > 20 && deltaTime1 < 50)
         {
             isDashing = false;
-            buttonAvailable = false;
             deltaTime1++;
-            cooldown.fillAmount += .04f;
         }
         if (deltaTime1 == 50)
         {
