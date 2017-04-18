92057599e4672bcd1505656540c3aa785f7eaf56
Lucas <GAR_dhlucas@seattleschools.org>
2017-04-18 11:59:23 -0700

    tank enemy working

--- Assets/Scripts/OpenWhenTouched.cs
+++ Assets/Scripts/OpenWhenTouched.cs
@@ -17,8 +17,9 @@ public class OpenWhenTouched : MonoBehaviour
         if (other.gameObject.CompareTag("Player")) { 
             GameObject itemChosen = GameObject.Find("ItemList").GetComponent<ItemList>().chooseRandoItem();
             
-            
+            transform.Translate(new Vector3(0, -.2f, 0));
             GameObject item = Instantiate(itemChosen, itemSpawn.transform.position, itemSpawn.transform.rotation) as GameObject;
+            
             GameObject chest = Instantiate(openedChest, this.transform.position, this.transform.rotation) as GameObject;
             Destroy(gameObject);
 
---
+++ Assets/TankTurretFiringAI.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankTurretFiringAI: MonoBehaviour
+{
+    private GameObject Player;
+    public GameObject bullet;
+    public GameObject shotSpawn;
+    public float shotDelay;
+    public float repeatRate;
+    private bool blocked;
+    public float shotSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+        InvokeRepeating("rotatoPotato", 0, .05f);
+        InvokeRepeating("LaunchProjectile", shotDelay, repeatRate);
+    }
+
+    void rotatoPotato()
+    {
+        if (this.GetComponent<EnemyAttributes>().room.GetComponent<PlayerInRoom>().InRoom)
+        {
+            transform.LookAt(Player.transform.position);
+            transform.Rotate(new Vector3(0, -90, 90), Space.Self);//correcting the original rotation
+                                                                  //transform.Rotate(new Vector3(0, -90, -90), Space.Self); // need to make y always rotato to 0!!!
+        }
+    }
+
+    void LaunchProjectile()
+    {
+        if (this.GetComponent<EnemyAttributes>().room.GetComponent<PlayerInRoom>().InRoom)
+        {
+            if (!blocked)
+            {
+                CircleCollider2D playerCollider = Player.GetComponent<CircleCollider2D>();
+
+                float angle = Mathf.Atan2(Player.transform.position.y - transform.position.y,
+                                           Player.transform.position.x - transform.position.x);
+                for (int i = -2; i < 3; i++)
+                {
+                    GameObject instance = Instantiate(bullet, shotSpawn.transform.position, shotSpawn.transform.rotation) as GameObject;
+                    Rigidbody2D rb2 = instance.GetComponent<Rigidbody2D>(); // rb2 is for the bullets
+                    rb2.velocity = new Vector2(Mathf.Cos(angle + (0.17453293f * i)) * shotSpeed, Mathf.Sin(angle + (0.17453293f * i)) * shotSpeed);
+                }
+            }
+        }
+    }
+}
+
