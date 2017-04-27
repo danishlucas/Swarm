9c0d082894efae464f3945cf5116ee20b34fd33c
Cameron <evilbees@comcast.net>
2017-04-27 11:47:02 -0700

    fixed enemy kill bug, and got score working

--- Assets/Scripts/KillWhenTouched.cs
+++ Assets/Scripts/KillWhenTouched.cs
@@ -6,11 +6,12 @@ using System;
 public class KillWhenTouched : MonoBehaviour
 {
 
-
+    private int done;
     GameObject room;
     void Start()
     {
         room = gameObject.transform.root.gameObject;
+        done = 0;
     }
 
     // Update is called once per frame
@@ -18,12 +19,24 @@ public class KillWhenTouched : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (other.GetComponent<PlayerController>().isDashing == true)
+            if (other.GetComponent<PlayerController>().isDashing == true && done == 0)
             {
+                done++;
+                if (GetComponentInParent<TankEnemyMovement>() != null)
+                {
+                    GameObject.Find("ScoreObject").GetComponent<Score>().changeScore(3);
+                } else if (GetComponentInParent<OmniTurretAI>() != null)
+                {
+                    GameObject.Find("ScoreObject").GetComponent<Score>().changeScore(1);
+                } else
+                {
+                    GameObject.Find("ScoreObject").GetComponent<Score>().changeScore(2);
+                }
                 gameObject.GetComponent<EnemyAttributes>().tryDropHealth();
                 Destroy(gameObject);
                 EnemyCounter script = room.GetComponent<EnemyCounter>();
                 script.enemyCount -= 1;
+
             }
         }
     }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public int score;
+
+    public Text countText;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        countText = GameObject.FindWithTag("CountText").GetComponent<Text>();
+        countText.text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DontDestroyOnLoad(this);
+        countText = GameObject.FindWithTag("CountText").GetComponent<Text>();
+        countText.text = score.ToString();
+	}
+
+    public void changeScore(int number)
+    {
+        score += number;
+    }
+}
