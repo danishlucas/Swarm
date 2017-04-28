4163661bc2060b98ab11a16f5a49b5a731cb9389
Cameron <evilbees@comcast.net>
2017-04-28 09:11:14 -0700

    score actually works now

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Score : MonoBehaviour {
 
     public int score;
-
+    //private Scene menu;
     public Text countText;
 	// Use this for initialization
 	void Start () {
         score = 0;
+        //menu = SceneManager.GetSceneByPath("Scenes/MenuScene.unity");
+        //Debug.Log(menu.name);
         countText = GameObject.FindWithTag("CountText").GetComponent<Text>();
         countText.text = score.ToString();
 	}
@@ -17,8 +20,15 @@ public class Score : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         DontDestroyOnLoad(this);
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "MenuScene")
+        {
+            Debug.Log("It do?");
+            Destroy(GameObject.Find("ScoreObject"));
+        }
         countText = GameObject.FindWithTag("CountText").GetComponent<Text>();
         countText.text = score.ToString();
+        
 	}
 
     public void changeScore(int number)
