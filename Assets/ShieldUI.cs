b6d5298aea5aecf86fec284d0cf503f417bf2227
Cameron Crawford <evilbees@comcast.net>
2017-05-18 11:53:26 -0700

    added a UI element for the shield

---
+++ Assets/ShieldUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShieldUI : MonoBehaviour {
+
+    public Sprite[] Images;
+    public Image Shield;
+    private GameObject Player;
+    bool shielded;
+
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("PLAYER");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Player.GetComponent<Attributes>().shielded)
+        {
+            Shield.sprite = Images[1];
+        } else
+        {
+            Shield.sprite = Images[0];
+        }
+	}
+}
