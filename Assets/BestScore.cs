386e3463f709d75fca1f7f751379af019e960ade
Cameron Crawford <evilbees@comcast.net>
2017-05-18 13:23:49 -0700

    High score now is implemented, and updates

---
+++ Assets/BestScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BestScore : MonoBehaviour {
+
+    public int highScore;
+
+	// Use this for initialization
+	void Start () {
+        highScore = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    DontDestroyOnLoad(this);
+	}
+}
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,13 +6,10 @@ using System.Collections;
 public class Score : MonoBehaviour {
 
     public float score;
-    //private Scene menu;
     public Text countText;
 	// Use this for initialization
 	void Start () {
         score = 0;
-        //menu = SceneManager.GetSceneByPath("Scenes/MenuScene.unity");
-        //Debug.Log(menu.name);
         countText = GameObject.FindWithTag("CountText").GetComponent<Text>();
         countText.text = score.ToString();
 	}
@@ -23,15 +20,26 @@ public class Score : MonoBehaviour {
         Scene scene = SceneManager.GetActiveScene();
         if (scene.name == "MenuScene")
         {
+            PlayerPrefs.Save();
             Destroy(GameObject.Find("ScoreObject"));
         }
         countText = GameObject.FindWithTag("CountText").GetComponent<Text>();
         countText.text = score.ToString();
-        
+        if (scene.name == "DeathScene")
+        {
+            if (PlayerPrefs.GetFloat("Temp Score") > PlayerPrefs.GetFloat("High Score"))
+            {
+                PlayerPrefs.SetFloat("High Score", PlayerPrefs.GetFloat("Temp Score"));
+            }  
+            Text highScore = GameObject.FindWithTag("HighScore").GetComponent<Text>();
+            highScore.text = PlayerPrefs.GetFloat("High Score").ToString();
+        }
 	}
 
     public void changeScore(float number)
     {
         score += number;
+        PlayerPrefs.SetFloat("Temp Score", score);
+        Debug.Log(PlayerPrefs.GetFloat("Temp Score"));
     }
 }
