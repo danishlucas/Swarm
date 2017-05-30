458927bc76f7e07582f8deead2908feef64e915a
Schnelz <GAR_crschnelz@seattleschools.org>
2017-05-30 09:31:22 -0700

    sounds

--- Assets/Scripts/EnemyCounter.cs
+++ Assets/Scripts/EnemyCounter.cs
@@ -5,4 +5,20 @@
 public class EnemyCounter : MonoBehaviour
 {
     public int enemyCount;
+    private int timesPlayed;
+
+    void Start()
+    {
+        timesPlayed = 0;
+    }
+
+    void Update()
+    {
+        if (enemyCount == 0 & timesPlayed == 0)
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            timesPlayed++;
+        }
+    }
 }
